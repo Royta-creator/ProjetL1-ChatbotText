359ee22443d56a996bb19e99fb368d5a8da60829
Royta-creator <126248833+Royta-creator@users.noreply.github.com>
2024-01-10 23:53:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,19 @@
-import os
-import re
-def list_of_files(directory, extension):
- 
-    files_names = []
-    for filename in os.listdir(directory):
-        if filename.endswith(extension) and '2' not in filename:
-            files_names.append(filename)
-    
-    return files_names
-
-def list_of_files2(directory, extension):
- 
-    files_names2 = []
-    for filename in os.listdir(directory):
-        if filename.endswith(extension):
-            files_names2.append(filename)
-    
-    return files_names2
-
+from functions import *
 directory = "./speeches"
 files_names = list_of_files(directory, "txt")
 files_names2 = list_of_files2(directory, "txt")
-
 DictName = {
-    "Chirac": "Jacque",
-    "Giscard": "Valery",
+    "Chirac": "Jacques",
+    "Giscard D'Estaing": "Valery",
     "Hollande": "François",
     "Macron": "Emmanuel",
     "Mitterrand": "François",
     "Sarkozy": "Nicolas"
 }
-def findName(name):
-    for key, values in DictName.items():
-        if key in name:
-            return values, key
-
-ListFirstNames = [findName(files_names[i]) for i in range(len(files_names))]
-
-print(ListFirstNames)
-
-def cleaned():
-    for i in range(len(files_names2)):
-        with open("speeches/{}".format(str(files_names2[i])), "r", encoding='utf-8') as file:
-            TextContent = nettoyer_texte((file.read()).lower())
-        with open("Cleaned/{}".format(files_names2[i]), "w", encoding='utf-8') as file1:
-            file1.write(TextContent)
-
-def nettoyer_texte(text):
-    text = re.sub(r'-|,|\'|\"|\.|\!|:|  ', ' ', text)
-    return text
-
+DictFrequence = FrequenceDansToutText(files_names2)
+DictioIDF, Toutmot = DictIDF(files_names2)
+TFIDF = DictTFIDF(files_names2, DictFrequence, DictioIDF, Toutmot)    
+matrix = [[0 for _ in Toutmot] for _ in files_names2]
+lst = [ListeNomsPresident(files_names, DictName), print_zero_tfidf_words(TFIDF), print_highest_tfidf_words(TFIDF)]
 if __name__ == '__main__':
-    cleaned()
-
-
-
+    chatbot(lst)
